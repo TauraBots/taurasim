9a667f97f82077609f8684b32bbf99cd7356c740
Thassio <thxssio@gmail.com>
2024-03-10 16:34:10 -0300

    Created Workspace vsss simulation

---
+++ TauraSim/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/thxssio/projects/VSSS/TauraSim/src
+# Build directory: /home/thxssio/projects/VSSS/TauraSim/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ TauraSim/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/thxssio/projects/VSSS/TauraSim/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/thxssio/projects/matrice_ws/devel;/opt/ros/noetic'.split(';') if '/home/thxssio/projects/matrice_ws/devel;/opt/ros/noetic' != '' else []
